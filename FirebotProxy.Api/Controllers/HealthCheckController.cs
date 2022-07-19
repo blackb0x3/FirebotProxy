ea8ef3fdc1449124605339bb1805d8a161ffeb98
Black Box <developer.luke.barker3@outlook.com>
2022-07-20 00:44:47 +0100

    Initial commit

---
+++ FirebotProxy.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirebotProxy.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    private readonly ILogger<HealthCheckController> _logger;
+
+    public HealthCheckController(ILogger<HealthCheckController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IResult HealthCheck()
+    {
+        return Results.Ok("OK");
+    }
+}
