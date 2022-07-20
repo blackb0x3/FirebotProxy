794e8dcd8f94d3c18c4cfe23e8fc6e3a09bca712
Black Box <developer.luke.barker3@outlook.com>
2022-07-20 16:41:47 +0100

    Firebot proxy commands controller

---
+++ FirebotProxy.Api/Controllers/CommandsController.cs
@@ -0,0 +1,30 @@
+﻿using FirebotProxy.Domain.PrimaryPorts.GetViewerChatRank;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirebotProxy.Api.Controllers;
+
+public class CommandsController : ProxyControllerBase
+{
+    private readonly ILogger<CommandsController> _logger;
+    private readonly IMediator _mediator;
+
+    public CommandsController(ILogger<CommandsController> logger, IMediator mediator)
+    {
+        _logger = logger;
+        _mediator = mediator;
+    }
+
+    [HttpGet("ChatRank/{viewerUsername}")]
+    public async Task<IResult> ChatRank(string viewerUsername)
+    {
+        var request = new GetViewerChatRankRequest { ViewerUsername = viewerUsername };
+
+        var response = await _mediator.Send(request);
+
+        return response.Match(
+            result => Results.Ok(result.ChatRankUrl),
+            failure => Results.Problem(failure.Message, statusCode: 500)
+        );
+    }
+}
