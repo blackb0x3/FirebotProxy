a6851d864c49ddcf1f190106fec750a57ea4ec2c
Black Box <developer.luke.barker3@outlook.com>
2022-07-20 02:18:35 +0100

    IoC setup for infrastructure project

---
+++ FirebotProxy.Infrastructure/IoC/InfrastructureInstaller.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FirebotProxy.Infrastructure.IoC;
+
+public class InfrastructureInstaller
+{
+    private static readonly Assembly InfrastructureProjectAssembly = typeof(InfrastructureInstaller).Assembly;
+
+    public static void Install(IServiceCollection services)
+    {
+        services.AddMediatR(InfrastructureProjectAssembly);
+    }
+}
