a1ef87975320a16c90598a1f09207f734e05973d
Black Box <developer.luke.barker3@outlook.com>
2022-08-07 15:38:05 +0100

    Add Initial_Create migration

---
+++ FirebotProxy.Data.Access/Migrations/20220807124312_Initial_Create.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirebotProxy.Data.Access.Migrations
+{
+    public partial class Initial_Create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatMessages",
+                columns: table => new
+                {
+                    SenderUsername = table.Column<string>(type: "TEXT", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatMessages_SenderUsername",
+                table: "ChatMessages",
+                column: "SenderUsername");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatMessages_Timestamp",
+                table: "ChatMessages",
+                column: "Timestamp");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatMessages");
+        }
+    }
+}
