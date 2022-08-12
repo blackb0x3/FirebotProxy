aaeb65ff680f62864ec4b6f8c82645c2f47dd330
Black Box <developer.luke.barker3@outlook.com>
2022-08-12 16:47:19 +0100

    Add helper method to translate numeric ranks to text-based ones

---
+++ FirebotProxy.Helpers/RankPositionHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace FirebotProxy.Helpers;
+
+public class RankPositionHelper
+{
+    /// <summary>
+    /// Gets string-ified rank position from an integer. E.g. 1 to 1st, 2 to 2nd, 3 to 3rd, 4 to 4th, 11 to 11th, 23 to 23rd etc.
+    /// </summary>
+    /// <param name="numericPosition"></param>
+    /// <returns></returns>
+    public static string GetRankPositionFromInteger(int numericPosition)
+    {
+        if (numericPosition <= 0)
+        {
+            throw new ArgumentException("Not a valid rank position integer. Should be greater than 0.", nameof(numericPosition));
+        }
+
+        switch (numericPosition % 100)
+        {
+            case 11:
+            case 12:
+            case 13:
+                return $"{numericPosition}th";
+        }
+
+        switch (numericPosition % 10)
+        {
+            case 1:
+                return $"{numericPosition}st";
+            case 2:
+                return $"{numericPosition}nd";
+            case 3:
+                return $"{numericPosition}rd";
+            default:
+                return $"{numericPosition}th";
+        }
+    }
+}
