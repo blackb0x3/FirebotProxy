3e17592ec02a0ed397ae660c3c623007364f1b49
Black Box <developer.luke.barker3@outlook.com>
2022-08-12 17:13:51 +0100

    Add validation representation

---
+++ FirebotProxy.Domain/Representations/ValidationRepresentation.cs
@@ -0,0 +1,11 @@
+﻿namespace FirebotProxy.Domain.Representations;
+
+public class ValidationRepresentation
+{
+    public ValidationRepresentation(FluentValidation.Results.ValidationResult result)
+    {
+        Errors = result.Errors.Select(e => $"{e.PropertyName} : {e.ErrorMessage}").ToArray();
+    }
+
+    public IReadOnlyCollection<string> Errors { get; set; }
+}
