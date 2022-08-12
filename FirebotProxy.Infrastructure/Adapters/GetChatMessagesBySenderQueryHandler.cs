0f81fc8eec60ebfb6038c02c9a884e313c611220
Black Box <developer.luke.barker3@outlook.com>
2022-08-12 13:15:15 +0100

    Implement GetChatMessagesBySender query

---
+++ FirebotProxy.Infrastructure/Adapters/GetChatMessagesBySenderQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using FirebotProxy.Data.Access;
+using FirebotProxy.Data.Entities;
+using FirebotProxy.SecondaryPorts.GetChatMessages;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace FirebotProxy.Infrastructure.Adapters;
+
+internal class GetChatMessagesBySenderQueryHandler : IRequestHandler<GetChatMessagesBySenderQuery, IReadOnlyCollection<ChatMessage>>
+{
+    private readonly ILogger<GetChatMessagesBySenderQueryHandler> _logger;
+    private readonly FirebotProxyContext _context;
+
+    public GetChatMessagesBySenderQueryHandler(ILogger<GetChatMessagesBySenderQueryHandler> logger, FirebotProxyContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public async Task<IReadOnlyCollection<ChatMessage>> Handle(GetChatMessagesBySenderQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.ChatMessages
+            .Where(cm => string.Equals(request.SenderUsername, cm.SenderUsername))
+            .ToListAsync(cancellationToken: cancellationToken);
+    }
+}
---
+++ FirebotProxy.SecondaryPorts/GetChatMessagesBySender/GetChatMessagesBySenderQuery.cs
@@ -0,0 +1,9 @@
+﻿using FirebotProxy.Data.Entities;
+using MediatR;
+
+namespace FirebotProxy.SecondaryPorts.GetChatMessages;
+
+public class GetChatMessagesBySenderQuery : IRequest<IReadOnlyCollection<ChatMessage>>
+{
+    public string SenderUsername { get; set; }
+}
