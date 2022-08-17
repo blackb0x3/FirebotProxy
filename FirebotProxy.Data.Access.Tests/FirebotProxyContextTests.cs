9d0f9852d68940d4d51140f756f87805846a2756
Black Box <developer.luke.barker3@outlook.com>
2022-08-17 14:38:01 +0100

    Add tests for FirebotProxy DB context

---
+++ FirebotProxy.Data.Access.Tests/FirebotProxyContextTests.cs
@@ -0,0 +1,53 @@
+﻿using FirebotProxy.Data.Entities;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace FirebotProxy.Data.Access.Tests;
+
+[TestFixture]
+public class A_FirebotProxy_Database_Context
+{
+    private FirebotProxyContext _context;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        var options = new DbContextOptionsBuilder<FirebotProxyContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        _context = new FirebotProxyContext(options);
+    }
+
+    [Test]
+    public async Task Updates_Created_And_LastUpdated_Timestamps_When_Entity_Created()
+    {
+        var entityToStore = new ChatMessage { Content = Guid.NewGuid().ToString(), Timestamp = DateTime.UtcNow, SenderUsername = "blah" };
+
+        await _context.ChatMessages.AddAsync(entityToStore);
+        await _context.SaveChangesAsync();
+
+        entityToStore.Created.Should().Be(entityToStore.LastUpdated);
+    }
+
+    [Test]
+    public async Task Updates_Only_The_LastUpdated_Timestamp_When_Entity_Modified()
+    {
+        // add the entity
+        var entityToStore = new ChatMessage { Content = Guid.NewGuid().ToString(), Timestamp = DateTime.UtcNow, SenderUsername = "blah" };
+
+        await _context.ChatMessages.AddAsync(entityToStore);
+        await _context.SaveChangesAsync();
+
+        // allow some time to elapse to ensure the dates will be different
+        await Task.Delay(100);
+
+        // now edit something
+        entityToStore.Content = Guid.NewGuid().ToString();
+
+        await _context.SaveChangesAsync();
+
+        entityToStore.Created.Should().BeBefore(entityToStore.LastUpdated);
+    }
+}
