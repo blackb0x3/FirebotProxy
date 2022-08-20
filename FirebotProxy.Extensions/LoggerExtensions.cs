21af3cd1fddfe2a565a01d936ae426166a5a6e6b
Black Box <developer.luke.barker3@outlook.com>
2022-08-20 15:44:47 +0100

    Add extension methods for Microsoft logging package

---
+++ FirebotProxy.Extensions/LoggerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace FirebotProxy.Extensions;
+
+public static class LoggerExtensions
+{
+    public static void LogDebug(this ILogger log, object values) => log.LogDebug("{@values}", values);
+
+    public static void LogInfo(this ILogger log, object values) => log.LogInformation("{@values}", values);
+
+    public static void LogWarning(this ILogger log, object values) => log.LogWarning("{@values}", values);
+
+    public static void LogError(this ILogger log, object values) => log.LogError("{@values}", values);
+}
