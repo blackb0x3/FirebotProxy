5569c2947956fd78843d17a1b9889db9da69559c
Black Box <developer.luke.barker3@outlook.com>
2022-08-20 20:30:21 +0100

    Implement random date generation

---
+++ FirebotProxy.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace FirebotProxy.Extensions;
+
+public static class DateTimeExtensions
+{
+    private static readonly Random Rnd = new();
+
+    public static DateTime Random()
+    {
+        var start = DateTime.UtcNow.AddMonths(-1);
+        var range = (DateTime.Today - start).Days; 
+
+        return start.AddDays(Rnd.Next(range));
+    }
+}
