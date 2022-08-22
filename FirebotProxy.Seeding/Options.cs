b034081b6b6b54e9c3f86014832f469d3ecacd2b
Black Box <developer.luke.barker3@outlook.com>
2022-08-22 19:23:41 +0100

    Add basic seeding project for local testing purposes

---
+++ FirebotProxy.Seeding/Options.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace FirebotProxy.Seeding;
+
+public class Options
+{
+    [Option("amount", Required = true, HelpText = "Number of messages to seed.")]
+    public int AmountToGenerate { get; set; }
+
+    [Option("start-from", Required = false, HelpText = "Earliest date for message timestamps.")]
+    public DateTime? EarliestTimestamp { get; set; }
+
+    [Option("audience-size", Required = false, Default = 10)]
+    public int AudienceSize { get; set; }
+}
---
+++ FirebotProxy.Seeding/Program.cs
@@ -0,0 +1,33 @@
+﻿using AutoBogus;
+using CommandLine;
+using FirebotProxy.Data.Access;
+using FirebotProxy.Data.Entities;
+using FirebotProxy.Seeding;
+
+Console.WriteLine("Hello, World!");
+
+var ctxFactory = new FirebotProxyContextFactory();
+var ctx = ctxFactory.CreateDbContext(args);
+
+await Parser.Default
+    .ParseArguments<Options>(args)
+    .WithParsedAsync(RunOptions);
+
+async Task RunOptions(Options options)
+{
+    var usernameFaker = new Bogus.DataSets.Internet();
+    var usernamesToPickFrom = new List<string>();
+
+    for (var i = 0; i < options.AudienceSize; i++)
+    {
+        usernamesToPickFrom.Add(usernameFaker.UserName());
+    }
+
+    var msgFaker = new AutoFaker<ChatMessage>();
+    msgFaker.RuleFor(cm => cm.SenderUsername, faker => faker.PickRandom(usernamesToPickFrom));
+    msgFaker.RuleFor(cm => cm.Timestamp, faker => faker.Date.Between(options.EarliestTimestamp ?? DateTime.Today.AddMonths(-1), DateTime.Today));
+    msgFaker.RuleFor(cm => cm.Content, faker => faker.Lorem.Letter(faker.Random.Number(1, 500)));
+
+    await ctx.AddRangeAsync(msgFaker.GenerateLazy(options.AmountToGenerate));
+    await ctx.SaveChangesAsync();
+}
