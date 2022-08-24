08a26883556f1136f4ec32211971395b1d097f2f
Black Box <developer.luke.barker3@outlook.com>
2022-08-24 18:00:52 +0100

    Add enum extension to get description attribute

---
+++ FirebotProxy.Extensions/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace FirebotProxy.Extensions;
+
+public static class EnumExtensions
+{
+    public static string GetDescription<T>(this T e) where T : Enum
+    {
+        var fi = e.GetType().GetField(e.ToString());
+
+        var attributes = (DescriptionAttribute[])fi?.GetCustomAttributes(typeof(DescriptionAttribute), false)!;
+
+        return attributes.Length > 0
+            ? attributes[0].Description
+            : e.ToString();
+    }
+}
