b6e05d0bc55bffce8a0658a580bce117d4929cbd
Black Box <developer.luke.barker3@outlook.com>
2022-08-24 18:00:31 +0100

    Add validation project to solution

---
+++ FirebotProxy.Validation/IsEnumMemberPropertyValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+
+namespace FirebotProxy.Validation;
+
+public class IsEnumMemberPropertyValidator<TToValidate, TEnum> : PropertyValidator<TToValidate, string> where TEnum : struct
+{
+    public override bool IsValid(ValidationContext<TToValidate> context, string value)
+    {
+        return Enum.TryParse<TEnum>(value, true, out _);
+    }
+
+    public override string Name => nameof(IsEnumMemberPropertyValidator<TToValidate, TEnum>);
+}
