86674eb38377b52f6822810ac1818564fe4208b4
Black Box <developer.luke.barker3@outlook.com>
2022-10-01 20:00:34 +0100

    Add tests for RemoveExpiredChatMessages request handler

---
+++ FirebotProxy.Domain.Tests/FakeHandlers/FakeRemoveExpiredChatMessagesCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using FirebotProxy.SecondaryPorts.RemoveExpiredChatMessages;
+using MediatR;
+
+namespace FirebotProxy.Domain.Tests.FakeHandlers;
+
+public class FakeRemoveExpiredChatMessagesCommandHandler : IRequestHandler<RemoveExpiredChatMessagesCommand>
+{
+    private readonly bool _shouldThrowException;
+
+    public FakeRemoveExpiredChatMessagesCommandHandler(bool shouldThrowException)
+    {
+        _shouldThrowException = shouldThrowException;
+    }
+
+    public async Task<Unit> Handle(RemoveExpiredChatMessagesCommand request, CancellationToken cancellationToken)
+    {
+        if (_shouldThrowException)
+        {
+            throw new Exception($"test exception message from {nameof(FakeRemoveMessagesByUsernameCommandHandler)}");
+        }
+
+        return await Unit.Task;
+    }
+}
---
+++ FirebotProxy.Domain.Tests/RemoveExpiredChatMessagesRequestHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using FirebotProxy.Domain.Adapters;
+using FirebotProxy.Domain.PrimaryPorts.RemoveExpiredChatMessages;
+using FirebotProxy.Domain.Representations;
+using FirebotProxy.Domain.Tests.FakeHandlers;
+using FirebotProxy.TestBase;
+using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using NUnit.Framework;
+
+namespace FirebotProxy.Domain.Tests;
+
+[TestFixture]
+public class A_RemoveExpiredChatMessages_Request_Handler_Removes_Expired_Messages
+{
+    [Test]
+    public async Task Successfully()
+    {
+        var logger = new NullLogger<RemoveExpiredChatMessagesRequestHandler>();
+
+        var mediator = new MediatRFactory(typeof(A_RemoveMessagesOfBannedViewer_Request_Handler_Removes_Messages).Assembly)
+            .AddSingletonHandler(new FakeRemoveExpiredChatMessagesCommandHandler(false));
+
+        var handler = new RemoveExpiredChatMessagesRequestHandler(logger, mediator.Build());
+
+        var response = await handler.Handle(new RemoveExpiredChatMessagesRequest(), CancellationToken.None);
+
+        response.Value.Should().BeOfType<RemoveExpiredChatMessagesSuccess>();
+    }
+}
+
+[TestFixture]
+public class A_RemoveExpiredChatMessages_Request_Handler_Does_Not_Remove_Expired_Messages
+{
+    [Test]
+    public async Task When_An_Exception_Is_Thrown()
+    {
+        var logger = new NullLogger<RemoveExpiredChatMessagesRequestHandler>();
+
+        var mediator = new MediatRFactory(typeof(A_RemoveMessagesOfBannedViewer_Request_Handler_Removes_Messages).Assembly)
+            .AddSingletonHandler(new FakeRemoveExpiredChatMessagesCommandHandler(true));
+
+        var handler = new RemoveExpiredChatMessagesRequestHandler(logger, mediator.Build());
+
+        var response = await handler.Handle(new RemoveExpiredChatMessagesRequest(), CancellationToken.None);
+
+        response.Value.Should().BeOfType<ErrorRepresentation>();
+        response.AsT1.Message.Should().Be("Failed to remove expired chat messages");
+    }
+}
