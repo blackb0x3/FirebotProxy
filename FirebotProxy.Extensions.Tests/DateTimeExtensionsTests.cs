282024cc54c8475901329f597b0a392b1f6b8cc2
Black Box <developer.luke.barker3@outlook.com>
2022-10-01 20:35:56 +0100

    Add DateTimeExtensions tests

---
+++ FirebotProxy.Extensions.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿namespace FirebotProxy.Extensions.Tests;
+
+[TestFixture]
+public class DateTime_Random_With_No_Params_Returns_A_Date
+{
+    /// <summary>
+    /// We emulate the parameters here to guarantee a success every time.
+    /// Otherwise, it generates the same as UtcNow, then we check UtcNow after
+    /// several ticks pass, and a failure gets thrown.
+    /// </summary>
+    [Test]
+    public void Between_Last_Month_And_Today()
+    {
+        var start = DateTime.UtcNow.AddMonths(-1);
+        var end = DateTime.Today;
+        var rndDate = DateTimeExtensions.Random(start, end);
+
+        rndDate.Should().BeOnOrBefore(end).And.BeOnOrAfter(start);
+    }
+}
+
+[TestFixture]
+public class DateTime_Random_With_Params_Returns_A_Date
+{
+    [Test]
+    [TestCaseSource(nameof(GetDates))]
+    public void Between_The_Start_And_End_Date(DateTime start, DateTime end)
+    {
+        var rndDate = DateTimeExtensions.Random(start, end);
+
+        rndDate.Should().BeOnOrBefore(end).And.BeOnOrAfter(start);
+    }
+
+    private static IEnumerable<object[]> GetDates()
+    {
+        var utcNow = DateTime.UtcNow;
+
+        yield return new object[] { utcNow.AddYears(-1), utcNow };
+        yield return new object[] { utcNow, utcNow }; // the equivalent of utcNow should get returned in this case
+    }
+}
+
+[TestFixture]
+public class DateTime_Random_With_Params_Throws_An_Exception
+{
+    [Test]
+    public void When_Start_Is_Greater_Than_End()
+    {
+        var start = DateTime.Today;
+        var end = DateTime.UtcNow.AddMonths(-1);
+
+        var act = () => DateTimeExtensions.Random(start, end);
+        act.Should().Throw<Exception>().WithMessage("start date cannot be later than the end date");
+    }
+}
--- FirebotProxy.Extensions/DateTimeExtensions.cs
+++ FirebotProxy.Extensions/DateTimeExtensions.cs
@@ -6,9 +6,30 @@ public static class DateTimeExtensions
 
     public static DateTime Random()
     {
-        var start = DateTime.UtcNow.AddMonths(-1);
-        var range = (DateTime.Today - start).Days; 
+        return Random(DateTime.UtcNow.AddMonths(-1));
+    }
 
-        return start.AddDays(Rnd.Next(range));
+    public static DateTime Random(DateTime start)
+    {
+        return Random(start, DateTime.Today);
+    }
+
+    public static DateTime Random(DateTime start, DateTime end)
+    {
+        if (start > end)
+        {
+            throw new Exception("start date cannot be later than the end date");
+        }
+
+        // early exit if the 2 date times are exact, since either/or is the only option
+        if (start.Equals(end))
+        {
+            return start;
+        }
+
+        var range = (end - start).Days;
+        var rndDaysToAdd = Rnd.Next(range);
+
+        return start.AddDays(rndDaysToAdd);
     }
 }
