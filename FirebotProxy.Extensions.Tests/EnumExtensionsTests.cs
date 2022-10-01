d17200d2612bd20900c6cad2466257f2cd023464
Black Box <developer.luke.barker3@outlook.com>
2022-10-01 20:41:34 +0100

    Add EnumExtensions tests

---
+++ FirebotProxy.Extensions.Tests/EnumExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿namespace FirebotProxy.Extensions.Tests;
+
+public enum TestEnum
+{
+    TestEnumNoDescription,
+    [System.ComponentModel.Description("Test Enum With Description")]
+    TestEnumWithDescription,
+}
+
+[TestFixture]
+public class EnumExtensions_GetDescription_Describes_An_Enum
+{
+    [Test]
+    public void When_A_Description_Is_Present()
+    {
+        TestEnum.TestEnumWithDescription.GetDescription().Should().Be("Test Enum With Description");
+    }
+
+    [Test]
+    public void When_A_Description_Is_Not_Present()
+    {
+        TestEnum.TestEnumNoDescription.GetDescription().Should().Be("TestEnumNoDescription");
+    }
+}
