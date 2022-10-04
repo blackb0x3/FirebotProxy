d0a472d029fb019a2ea41071200b4f9442d0f481
Black Box <developer.luke.barker3@outlook.com>
2022-10-04 17:30:39 +0100

    Ignore seeding project when building via msbuild

--- FirebotProxy.Seeding/Program.cs
+++ FirebotProxy.Seeding/Program.cs
@@ -2,43 +2,50 @@
 using CommandLine;
 using FirebotProxy.Data.Access;
 using FirebotProxy.Data.Entities;
-using FirebotProxy.Seeding;
 using MoreLinq;
 
-Console.WriteLine("Hello, World!");
+namespace FirebotProxy.Seeding;
 
-var ctxFactory = new FirebotProxyContextFactory();
-var ctx = ctxFactory.CreateDbContext(args);
-
-await Parser.Default
-    .ParseArguments<Options>(args)
-    .WithParsedAsync(RunOptions);
-
-async Task RunOptions(Options options)
+public class Program
 {
-    var usernameFaker = new Bogus.DataSets.Internet();
-    var usernamesToPickFrom = new List<string>();
-
-    for (var i = 0; i < options.AudienceSize; i++)
+    public async Task Main(string[] args)
     {
-        usernamesToPickFrom.Add(usernameFaker.UserName());
+        Console.WriteLine("Hello, World!");
+
+        var ctxFactory = new FirebotProxyContextFactory();
+        var ctx = ctxFactory.CreateDbContext(args);
+
+        await Parser.Default
+            .ParseArguments<Options>(args)
+            .WithParsedAsync(async opts => await RunOptions(opts, ctx));
     }
 
-    var msgFaker = new AutoFaker<ChatMessage>();
-    msgFaker.RuleFor(cm => cm.SenderUsername, faker => faker.PickRandom(usernamesToPickFrom));
-    msgFaker.RuleFor(cm => cm.Timestamp, faker => faker.Date.Between(options.EarliestTimestamp ?? DateTime.Today.AddMonths(-1), DateTime.Today));
-    msgFaker.RuleFor(cm => cm.Content, faker => string.Join(". ", faker.Lorem.Sentences(faker.Random.Int(min: 1, max: 3))));
-
-    const int batchSize = 10000;
-    var totalBatches = options.AmountToGenerate / batchSize;
-    var batchCounter = 0;
-
-    foreach (var batch in msgFaker.GenerateLazy(options.AmountToGenerate).Batch(batchSize))
+    private async Task RunOptions(Options options, FirebotProxyContext ctx)
     {
-        Console.WriteLine($"Batch {batchCounter++} of {totalBatches}");
-        await ctx.AddRangeAsync(batch);
-        await ctx.SaveChangesAsync();
-        Console.WriteLine("Batch saved");
-        Console.WriteLine(new string('-', Console.BufferWidth / 4));
+        var usernameFaker = new Bogus.DataSets.Internet();
+        var usernamesToPickFrom = new List<string>();
+
+        for (var i = 0; i < options.AudienceSize; i++)
+        {
+            usernamesToPickFrom.Add(usernameFaker.UserName());
+        }
+
+        var msgFaker = new AutoFaker<ChatMessage>();
+        msgFaker.RuleFor(cm => cm.SenderUsername, faker => faker.PickRandom(usernamesToPickFrom));
+        msgFaker.RuleFor(cm => cm.Timestamp, faker => faker.Date.Between(options.EarliestTimestamp ?? DateTime.Today.AddMonths(-1), DateTime.Today));
+        msgFaker.RuleFor(cm => cm.Content, faker => string.Join(". ", faker.Lorem.Sentences(faker.Random.Int(min: 1, max: 3))));
+
+        const int batchSize = 10000;
+        var totalBatches = options.AmountToGenerate / batchSize;
+        var batchCounter = 0;
+
+        foreach (var batch in msgFaker.GenerateLazy(options.AmountToGenerate).Batch(batchSize))
+        {
+            Console.WriteLine($"Batch {batchCounter++} of {totalBatches}");
+            await ctx.AddRangeAsync(batch);
+            await ctx.SaveChangesAsync();
+            Console.WriteLine("Batch saved");
+            Console.WriteLine(new string('-', Console.BufferWidth / 4));
+        }
     }
 }
