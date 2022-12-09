3f9594d1278b0057aeed08d8ead84a77d6831d94
Black Box <developer.luke.barker3@outlook.com>
2022-12-09 22:29:22 +0000

    Implement ShortenUrl command

---
+++ FirebotProxy.Infrastructure/Adapters/ShortenUrlCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using FirebotProxy.Infrastructure.Services;
+using FirebotProxy.SecondaryPorts.ShortenUrl;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace FirebotProxy.Infrastructure.Adapters;
+
+public class ShortenUrlCommandHandler : IRequestHandler<ShortenUrlCommand, string>
+{
+    private readonly ILogger<ShortenUrlCommandHandler> _logger;
+    private readonly IHideUrlApi _hideUrlApi;
+
+    public ShortenUrlCommandHandler(ILogger<ShortenUrlCommandHandler> logger, IHideUrlApi hideUrlApi)
+    {
+        _logger = logger;
+        _hideUrlApi = hideUrlApi;
+    }
+
+    public async Task<string> Handle(ShortenUrlCommand request, CancellationToken cancellationToken)
+    {
+        var resp = await _hideUrlApi.ShortenUrl(new ShortenUrlRequestDto { UrlToShorten = request.UrlToShorten });
+
+        return resp.ShortenedUrl;
+    }
+}
---
+++ FirebotProxy.SecondaryPorts/ShortenUrl/ShortenUrlCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace FirebotProxy.SecondaryPorts.ShortenUrl;
+
+public class ShortenUrlCommand : IRequest<string>
+{
+    public string UrlToShorten { get; set; }
+}
